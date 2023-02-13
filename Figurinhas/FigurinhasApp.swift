f863be3cbd541bb952854aa249d8bbcacc12a880
Douglas Nascimento <115051705+douglasfnascimento@users.noreply.github.com>
2023-02-13 16:27:50 -0300

    Initial Commit

---
+++ Figurinhas/FigurinhasApp.swift
@@ -0,0 +1,17 @@
+//
+//  FigurinhasApp.swift
+//  Figurinhas
+//
+//  Created by Douglas Nascimento on 13/02/23.
+//
+
+import SwiftUI
+
+@main
+struct FigurinhasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
